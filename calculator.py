2e7cfe65eff5761157ae1a1ac339ee6a77ca114f
Ettigowni Sneha <ettigownisneha@gmail.com>
2026-08-20 11:47:35 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+first_number=int(input("enter your first number"))
+second_number=int(input("enter your second number"))
+
+addition = first_number + second_number
+print(addition)
+
+subtraction = first_number - second_number
+print(subtraction)
+
+multiplication = first_number * second_number
+print(multiplication)
+
+division = first_number / second_number
+print(division)
